983b2f126be323f1d1c56674077a1dc0ea8c434a
Nicola <7451194+nicolapasqua99@users.noreply.github.com>
2023-09-24 14:18:34 +0200

    switched to portfolio np firebase project

--- app/check-dict/page.tsx
+++ app/check-dict/page.tsx
@@ -1,14 +1,6 @@
-import {child, get, getDatabase, onValue, ref} from 'firebase/database'
-import firebase_app from '../../firebase/config'
 import './page.css'
 
 export default function Home() {
-    const db = getDatabase(firebase_app)
-    const dbRef = ref(db)
-    let dbValues = {}
-
-    onValue(dbRef, (val) => (dbValues = val.val()))
-
     return (
         <main>
             <h1>CHECK DICTIONARIES</h1>
