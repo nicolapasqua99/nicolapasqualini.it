ef3302d9d217f13c1871e8c47460a72d268092ab
Nicola <7451194+nicolapasqua99@users.noreply.github.com>
2023-09-25 19:00:19 +0200

    Rewrote SEO description message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import './globals.css'
-import {Quicksand} from 'next/font/google'
+import "./globals.css";
+import { Quicksand } from "next/font/google";
 
-const quicksand = Quicksand({subsets: ['latin'], weight: 'variable'})
+const quicksand = Quicksand({ subsets: ["latin"], weight: "variable" });
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
-    return (
-        <html lang='en'>
-            <head>
-                <link
-                    rel='icon'
-                    href='/name.png'
-                    sizes='any'
-                /> 
-    <title>Nicola Pasqualini</title>
-    <meta name="description" content="I am a developer! "/>
-
-            </head>
-            <body className={quicksand.className}>{children}</body>
-        </html>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/name.png" sizes="any" />
+        <title>Nicola Pasqualini</title>
+        <meta name="description" content="Hi! I'm Nicola Pasqualini and I love to develop software or design interfaces and think about how they will help people. My biggest passion is to get myself and my work every day better."/>
+      </head>
+      <body className={quicksand.className}>{children}</body>
+    </html>
+  );
 }
