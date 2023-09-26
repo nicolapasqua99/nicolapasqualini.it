93612705a8854436a52872a1569d05000ac0e9e0
Nicola <7451194+nicolapasqua99@users.noreply.github.com>
2023-09-26 10:52:35 +0200

    Prepared pariginini page

---
+++ app/pariginini/page.tsx
@@ -0,0 +1,11 @@
+import './page.css'
+
+export default function Home() {
+    return (
+        <main>
+            <div class="title">
+                <h1>PROVA</h1>
+            </div>
+        </main>
+    )
+}
