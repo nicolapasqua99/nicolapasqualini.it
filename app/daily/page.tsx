25a1a6db0d5d53f3e2a4d2b0b6ed4db55bdea050
Nicola <7451194+nicolapasqua99@users.noreply.github.com>
2023-09-27 21:34:07 +0200

    fix package.json and type definition

--- app/daily/page.tsx
+++ app/daily/page.tsx
@@ -1,45 +1,44 @@
+'use client'
 
-"use client"
-
-import "./page.css"
-import styled from "styled-components"
-import { saveAs } from "file-saver"
+import './page.css'
+import styled from 'styled-components'
+// import { saveAs } from "file-saver"
 // import { DragDropFile } from './dragdrop'
 import Person from './person'
 
 const Title = styled.h1`
-  position: fixed;
-  top: 8px;
-  left: 5vw;
-  font-size: 24px;
-  text-align: center;
+    position: fixed;
+    top: 8px;
+    left: 5vw;
+    font-size: 24px;
+    text-align: center;
 `
 
 const DownloadBtn = styled.button`
-  position: fixed;
-  margin-top: 24px;
-  right: 5vw;
-  font-size: 16px;
-  text-align: center;
-  padding: 4px 12px 4px 12px;
-  background-color: white;
-  color: black;
-  cursor: pointer;
-  &:hover{
-    background-color: black;
-    color: white;
-  }
+    position: fixed;
+    margin-top: 24px;
+    right: 5vw;
+    font-size: 16px;
+    text-align: center;
+    padding: 4px 12px 4px 12px;
+    background-color: white;
+    color: black;
+    cursor: pointer;
+    &:hover {
+        background-color: black;
+        color: white;
+    }
 `
 
 const PersonBox = styled.div`
-  width: 90vw;
-  position: fixed;
-  bottom: 5vh;
-  left: 5vw;
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-`;
+    width: 90vw;
+    position: fixed;
+    bottom: 5vh;
+    left: 5vw;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+`
 
 const persons = ['AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA']
 
@@ -50,16 +49,16 @@ const persons = ['AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA', 
 // }
 
 export default function Home() {
-  return (
-    <main>
-      <Title>Sprint nr: 68</Title>
-      {/* <DownloadBtn onClick={download}>export</DownloadBtn> */}
-      <PersonBox>
-      {persons.map((person, index) => 
-        <Person key={index} person={person} />
-      )}
-      </PersonBox>
-      {/* <DragDropFile></DragDropFile> */}
-    </main>
-  );
+    return (
+        <main>
+            <Title>Sprint nr: 68</Title>
+            {/* <DownloadBtn onClick={download}>export</DownloadBtn> */}
+            <PersonBox>
+                {persons.map((person, index) => (
+                    <Person key={index} person={person} />
+                ))}
+            </PersonBox>
+            {/* <DragDropFile></DragDropFile> */}
+        </main>
+    )
 }
--- app/daily/person.tsx
+++ app/daily/person.tsx
@@ -1,26 +1,30 @@
-"use client"
+'use client'
 
-import { useState } from "react";
-import styled from "styled-components";
+import { useState } from 'react'
+import styled from 'styled-components'
 
-const PersonContainer = styled.p`
-  width: calc(10vw - 12px);
-  height: 32px;
-  line-height: 32px;
-  text-align: center;
-  margin: 0 4px 0 4px;
-  border: 2px solid black;
-  cursor: pointer;
-  transition: .2s ease all;
-  background-color: ${props => props.talks === 'true' ? 'white' : 'black'};
-  color: ${props => props.talks === 'true' ? 'black' : 'white'};
-  ::selection{
-    background-color: ${props => props.talks === 'true' ? 'white' : 'black'};
-    color: ${props => props.talks === 'true' ? 'black' : 'white'};
-  }
-`;
+const PersonContainer = styled.p<{ talks: boolean }>`
+    width: calc(10vw - 12px);
+    height: 32px;
+    line-height: 32px;
+    text-align: center;
+    margin: 0 4px 0 4px;
+    border: 2px solid black;
+    cursor: pointer;
+    transition: 0.2s ease all;
+    background-color: ${props => (props.talks ? 'white' : 'black')};
+    color: ${props => (props.talks ? 'black' : 'white')};
+    ::selection {
+        background-color: ${props => (props.talks ? 'white' : 'black')};
+        color: ${props => (props.talks ? 'black' : 'white')};
+    }
+`
 
-export default function Person(props) {
-  const [talked, setTalked] = useState(false);
-  return <PersonContainer talks={talked.toString()} onClick={() => setTalked(!talked)}>{props.person}</PersonContainer>;
+export default function Person(props: any) {
+    const [talked, setTalked] = useState(false)
+    return (
+        <PersonContainer talks={talked} onClick={() => setTalked(!talked)}>
+            {props.person}
+        </PersonContainer>
+    )
 }
