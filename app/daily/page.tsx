acf261ed3f5547949454a01a024a4292a113deba
Nicola Pasqualini <nicolapasqua99@gmail.com>
2023-12-16 19:21:54 +0200

    Change daily tool to bubble style

--- app/daily/page.tsx
+++ app/daily/page.tsx
@@ -4,45 +4,93 @@ import './page.css'
 import styled from 'styled-components'
 import Person from './person'
 
+const Scrollable = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow-y: scroll; 
+`
+
 const Title = styled.h1`
     font-size: 56px;
     text-align: center;
-    width: 100%;
+    width: auto;
+    text-transform: uppercase;
+    padding: 8px;
 `
 
 const TitleBox = styled.div`
     position: fixed;
-    top: 0;
+    top: calc(50vh - 28px);
     left: 0;
     width: 100vw;
-    height: 20vh;
+    height: 56px;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    justify-content: center;
+    z-index: 2;
+`
+
+const LateralTitleBox = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 56px;
+    z-index: 2
+`
+
+const LateralTitle = styled.h1`
+    font-size: 56px;
+    text-align: center;
+    width: 100vh;
+    height: 56px;
+    text-transform: uppercase;
+    transform: rotate(90deg) translateX(-56px);
+    transform-origin: left bottom;
 `
 
 const PersonBox = styled.div`
-    position: fixed;
-    top: 20vh;
-    left: 0;
-    width: 100vw;
-    height: 80vh;
+    position: relative;
+    top: 0vh;
+    left: 56px;
+    width: calc(100vw - 56px);
     display: flex;
     flex-flow: row wrap;
     justify-content: center;
     align-content: center;
+    z-index: 1;
 `
 
-const persons = ['AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA']
+const persons = [
+    'AAAAA', 
+    'BBBBB', 
+    'AAAAAAAAAAAA AAAAAAAAAAAAAAAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA', 'BBBBB', 'AAAAA']
 
 export default function Home() {
     return (
         <main>
-            <TitleBox>
-                <Title>Sprint nr: 68</Title>
-            </TitleBox>
-            <PersonBox>
-                {persons.map((person, index) => (
-                    <Person key={index} person={person} x={index} y={index} />
-                    ))}
-            </PersonBox>
+            <Scrollable>
+                {/* <TitleBox>
+                    <Title>sprint daily</Title>
+                </TitleBox> */}
+                <LateralTitleBox>
+                    <LateralTitle>sprint daily</LateralTitle>
+                </LateralTitleBox>
+                <PersonBox>
+                    {persons.map((person, index) => (
+                        <Person key={index} person={person} x={index} y={index} />
+                        ))}
+                </PersonBox>
+                <PersonBox>
+                    {persons.map((person, index) => (
+                        <Person key={index} person={person} x={index} y={index} />
+                        ))}
+                </PersonBox>
+            </Scrollable>
         </main>
     )
 }
--- app/daily/person.tsx
+++ app/daily/person.tsx
@@ -3,20 +3,43 @@
 import { useState } from 'react'
 import styled from 'styled-components'
 
-const PersonContainer = styled.p<{ talks: boolean, x: number, y: number }>`
-    width: calc(10vw - 12px);
-    height: 32px;
-    line-height: 32px;
-    text-align: center;
-    margin: 0 4px 0 4px;
-    border: 2px solid black;
+const PersonContainer = styled.div<{ talks: boolean, x: number, y: number }>`
+    width: calc(15vw - 4px);
+    height: calc(15vw - 4px);
+    margin: 16px;
+    border-radius: 50%;
+    border: 2px solid white;
     cursor: pointer;
-    transition: 0.2s ease all;
-    background-color: ${props => (props.talks ? 'white' : 'black')};
-    color: ${props => (props.talks ? 'black' : 'white')};
+    transition: .4s ease all;
+    background-color: ${props => (props.talks ? 'rgb(0, 0, 0, .6)' : 'rgb(0, 0, 0, .2)')};
+    color: ${props => (props.talks ? 'rgb(255, 255, 255, .2)' : 'rgb(255, 255, 255, 1)')};
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    justify-content: center;
+    & span  {
+        position: absolute;
+        width: 50px;
+        height: 50px;
+        transition: opacity .4s ease;
+        clip-path: polygon(0% 70%, 30% 100%, 100% 30%, 85% 15%, 30% 70%, 15% 55%);
+        opacity: ${props => (props.talks ? '1' : '0')};
+        background-color: rgb(255, 255, 255);
+    }
+    & p {
+        position: absolute;
+        width: calc(15% - 4px);
+        height: calc(20% - 4px);
+        font-size: 24px;
+        display: flex;
+        flex-flow: row wrap;
+        align-items: center;
+        justify-content: center;
+        word-wrap: wrap;
+        text-align: center;
+    }
     ::selection {
-        background-color: ${props => (props.talks ? 'white' : 'black')};
-        color: ${props => (props.talks ? 'black' : 'white')};
+        color: ${props => (props.talks ? 'rgb(255, 255, 255, .2)' : 'rgb(255, 255, 255, 1)')};
     }
 `
 
@@ -24,7 +47,8 @@ export default function Person(props: any) {
     const [talked, setTalked] = useState(false)
     return (
         <PersonContainer x={props.x} y={props.y} talks={talked} onClick={() => setTalked(!talked)}>
-            {props.person}
+            <span></span>
+            <p>{props.person}</p>
         </PersonContainer>
     )
 }
