77f5073926ddeeeae580e3002ab357c9a2ce7200
Nicola <7451194+nicolapasqua99@users.noreply.github.com>
2024-02-14 11:40:02 +0100

    easy note page init

---
+++ app/easynote/page.tsx
@@ -0,0 +1,17 @@
+import './page.css'
+
+export default function Home() {
+
+    return (
+        <main>
+            <div className="sidebar_menu">
+            </div>
+            <div className="sidebar_navigator">
+
+            </div>
+            <div className="textarea_container">
+
+            </div>
+        </main>
+    )
+}
