2758998ff2153bd61bca160cafb382819902bfbe
Nicola <7451194+nicolapasqua99@users.noreply.github.com>
2024-02-14 11:52:29 +0100

    experiments

--- app/easynote/page.tsx
+++ app/easynote/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     return (
         <main>
             <div className="sidebar_menu">
+                <div>A</div>
+                <div>B</div>
+                <div>B</div>
+                <div>B</div>
             </div>
             <div className="sidebar_navigator">
 
