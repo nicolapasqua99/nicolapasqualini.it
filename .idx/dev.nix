58a3d5adaa2b8226c5401eee78d82655d03581a7
nicolapasqua99 <nicolapasqua99@gmail.com>
2024-05-24 09:57:51 +0000

    add idx configurations files

---
+++ .idx/dev.nix
@@ -0,0 +1,29 @@
+{ pkgs }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "github.vscode-github-actions"
+    "pr1sm8.theme-panda"
+  ];
+  idx.previews = {
+    enable = true;
+    previews = [
+      {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        id = "web";
+        manager = "web";
+      }
+    ];
+  };
+}
