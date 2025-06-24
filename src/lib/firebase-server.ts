b03e2477bfa935b6f53bf44e4fcb9c04b8f34754
Nicola Pasqualini <nicolapasqua99@gmail.com>
2025-06-24 12:08:26 +0200

    chore/added missing key

--- src/lib/firebase-server.ts
+++ src/lib/firebase-server.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp, cert } from 'firebase-admin/app'
+import { initializeApp, getApps, getApp, cert, App } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getDatabase } from 'firebase-admin/database'
@@ -6,21 +6,23 @@ import { getDatabase } from 'firebase-admin/database'
 const firebaseConfig = {
     credential: cert({
         projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-        privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        clientEmail: process.env.CLIENT_EMAIL
     })
 }
 
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig)
+function getAppInstance(): App {
+    return getApps().length > 0 ? getApp() : initializeApp(firebaseConfig)
+}
 
 export function getServerAuth() {
-    return getAuth(app)
+    return getAuth(getAppInstance())
 }
 
 export function getServerFirestore() {
-    return getFirestore(app)
+    return getFirestore(getAppInstance())
 }
 
 export function getServerRealtimeDatabase() {
-    return getDatabase(app)
+    return getDatabase(getAppInstance())
 }
