7e5b20cf9472cab36d30d8e9226ee4640e404b17
Nicola Pasqualini <nicolapasqua99@gmail.com>
2025-06-28 15:08:29 +0200

    fix/getting there with the workflow

--- src/lib/firebase-server.ts
+++ src/lib/firebase-server.ts
@@ -2,8 +2,14 @@ import { initializeApp, getApps, getApp, cert, App } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getDatabase } from 'firebase-admin/database'
+import { credential } from 'firebase-admin'
 
-const firebaseConfig = {}
+const serviceAccount = require("../../firebase-service-account.json")
+
+const firebaseConfig = {
+    credential: credential.cert(serviceAccount),
+    databaseURL: "https://nicolapasqualini-portfolio-default-rtdb.europe-west1.firebasedatabase.app"
+}
 
 function getAppInstance(): App {
     return getApps().length > 0 ? getApp() : initializeApp(firebaseConfig)
