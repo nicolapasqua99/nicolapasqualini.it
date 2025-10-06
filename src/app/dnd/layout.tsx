d44fd514f32a8b060ca3e245a4616261b96a7d7b
nicolapasqua99 <nicolapasqua99@gmail.com>
2025-10-06 16:52:26 +0200

    refactor/separated checkIfUserAuthorized method in checkIfUserAuthorizedOnVisiting and checkIfUserAuthorizedOnAPIRequest for dnd layout

--- src/app/dnd/layout.tsx
+++ src/app/dnd/layout.tsx
@@ -1,9 +1,9 @@
-import { checkIfUserAuthorized } from '@/src/lib/authorization'
+import { checkIfUserAuthorizedOnVisiting } from '@/src/lib/authorization'
 import { redirect } from 'next/navigation'
 
 export default async function DnDLayout({ children }: { children: React.ReactNode }) {
     try {
-        await checkIfUserAuthorized({ role: 'user', section: 'dnd' })
+        await checkIfUserAuthorizedOnVisiting({ role: 'user', section: 'dnd' })
     } catch (error) {
         redirect('/login')
     }
